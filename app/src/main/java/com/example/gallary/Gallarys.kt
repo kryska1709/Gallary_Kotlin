15983a51ce837f51055cbbc4679ff0fe61c7bcd0
Your Name <you@example.com>
2025-03-23 14:28:18 +0300

    сделала кнопки для перемещения и убрала диалоговое окно

--- app/src/main/java/com/example/gallary/Gallarys.kt
+++ app/src/main/java/com/example/gallary/Gallarys.kt
@@ -1,5 +1,6 @@
-package com.example.gallery
+package com.example.gallary
 
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -10,23 +11,27 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import com.example.gallary.R
+
 
 @Composable
 fun Gallarys() {
-    val selectedImage = remember { mutableStateOf<Int?>(null) }
+    val selectedIndexImage = remember { mutableStateOf<Int?>(null) }
+    val context = LocalContext.current
     val gallery = listOf(
         R.drawable.may,
         R.drawable.airo,
@@ -57,37 +62,73 @@ fun Gallarys() {
         R.drawable.zuki,
         R.drawable.zuko
     )
+    if(selectedIndexImage.value == null) {
 
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(4),
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        horizontalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        items(gallery) { imageId ->
-            GalleryPhoto(
-                imageId = imageId,
-                onClick = {
-                    selectedImage.value = imageId
-                }
-            )
-        }
-    }
-    selectedImage.value?.let {
-        AlertDialog(
-            onDismissRequest = { selectedImage.value },
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(4),
             modifier = Modifier.fillMaxSize(),
-            confirmButton = {
-                Button({ selectedImage.value = null }) {
-                    Text(text = "назад")
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(gallery.size) { index :Int ->
+                GalleryPhoto(
+                    imageId = gallery[index]
+                ){
+                    selectedIndexImage.value = index
                 }
-            },
-            text = {Image(
-                painter = painterResource(it),
+            }
+        }
+    } else {
+        Box(
+
+        ) {
+            Image(
+                painter = painterResource(id = gallery[selectedIndexImage.value!!]),
                 contentDescription = null,
-                modifier = Modifier.fillMaxSize(),
-            )}
-        )
+                modifier = Modifier
+                    .fillMaxSize()
+                    .align(alignment = Alignment.Center)
+            )
+            if (selectedIndexImage.value!! > 0) {
+                IconButton(
+                    { selectedIndexImage.value = selectedIndexImage.value!! - 1 },
+                    modifier = Modifier.align(alignment = Alignment.CenterStart)
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.left),
+                        contentDescription = null,
+                        tint = Color.Cyan,
+                    )
+                }
+            } else {
+                Toast.makeText(context, "вы на первой фотографии", Toast.LENGTH_SHORT).show()
+            }
+            if (selectedIndexImage.value!! < gallery.size - 1) {
+                IconButton(
+                    { selectedIndexImage.value = selectedIndexImage.value!! + 1 },
+                    modifier = Modifier.align(alignment = Alignment.CenterEnd)
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.right),
+                        contentDescription = null,
+                        tint = Color.Cyan
+                    )
+                }
+            } else {
+                Toast.makeText(context, "вы на последней фотографии", Toast.LENGTH_SHORT).show()
+            }
+
+            IconButton(
+                { selectedIndexImage.value = null },
+                modifier = Modifier.align(alignment = Alignment.BottomCenter)
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.down),
+                    contentDescription = null,
+                    tint = Color.Cyan
+                )
+            }
+        }
     }
 }
 
